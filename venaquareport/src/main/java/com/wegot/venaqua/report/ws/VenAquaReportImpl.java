553e7d62fb18e4c1420f89d226e755e10d1c9cde
karthik <karthik.p@volantetech.net>
2018-09-07 12:02:49 +0530

    Json Added.

--- venaquareport/src/main/java/com/wegot/venaqua/report/ws/VenAquaReportImpl.java
+++ venaquareport/src/main/java/com/wegot/venaqua/report/ws/VenAquaReportImpl.java
@@ -1,8 +1,7 @@
 package com.wegot.venaqua.report.ws;
 
 import com.wegot.venaqua.report.json.JSONConverter;
-import sun.misc.IOUtils;
-import sun.nio.ch.IOUtil;
+import org.apache.commons.io.IOUtils;
 
 import javax.jws.WebService;
 import java.io.IOException;
@@ -15,44 +14,135 @@ public class VenAquaReportImpl implements VenAquaReport {
     @Override
     public String getSiteConsumptionByWaterSource(String requestInfo) {
         String response = null;
-        System.out.println("Processing...");
+        System.out.println("**** Request Info ****");
+        System.out.println(requestInfo);
         try {
             RequestInfo requestInfoObj = JSONConverter.CovertToObject(requestInfo, RequestInfo.class);
-
+            System.out.println("**** Parsed Request Info ****");
             System.out.println("Uid : " + requestInfoObj.getUid());
             System.out.println("ChartType : " + requestInfoObj.getChartType());
             System.out.println("FromDate : " + requestInfoObj.getFromDate());
             System.out.println("ToDate : " + requestInfoObj.getToDate());
 
             InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("SiteConsumptionByWaterSource.json");
-            if(resourceAsStream!=null) {
-
+            if (resourceAsStream != null) {
+                response = IOUtils.toString(resourceAsStream);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
             response = e.getMessage();
         }
+        System.out.println("**** Response Info ****");
+        System.out.println(response);
         return response;
     }
 
     @Override
     public String getResidenceConsumption(String requestInfo) {
-        return "ResidenceConsumption";
+        String response = null;
+        System.out.println("**** Request Info ****");
+        System.out.println(requestInfo);
+        try {
+            RequestInfo requestInfoObj = JSONConverter.CovertToObject(requestInfo, RequestInfo.class);
+            System.out.println("**** Parsed Request Info ****");
+            System.out.println("Uid : " + requestInfoObj.getUid());
+            System.out.println("ChartType : " + requestInfoObj.getChartType());
+            System.out.println("FromDate : " + requestInfoObj.getFromDate());
+            System.out.println("ToDate : " + requestInfoObj.getToDate());
+
+            InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("ResidenceConsumption.json");
+            if (resourceAsStream != null) {
+                response = IOUtils.toString(resourceAsStream);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            response = e.getMessage();
+        }
+        System.out.println("**** Response Info ****");
+        System.out.println(response);
+        return response;
     }
 
     @Override
     public String getSiteDemandByWaterType(String requestInfo) {
-        return "SiteDemandByWaterType(";
+        String response = null;
+        System.out.println("**** Request Info ****");
+        System.out.println(requestInfo);
+        try {
+            RequestInfo requestInfoObj = JSONConverter.CovertToObject(requestInfo, RequestInfo.class);
+            System.out.println("**** Parsed Request Info ****");
+            System.out.println("Uid : " + requestInfoObj.getUid());
+            System.out.println("ChartType : " + requestInfoObj.getChartType());
+            System.out.println("FromDate : " + requestInfoObj.getFromDate());
+            System.out.println("ToDate : " + requestInfoObj.getToDate());
+
+            InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("SiteDemandByWaterType.json");
+            if (resourceAsStream != null) {
+                response = IOUtils.toString(resourceAsStream);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            response = e.getMessage();
+        }
+        System.out.println("**** Response Info ****");
+        System.out.println(response);
+        return response;
     }
 
     @Override
     public String getPumpStateAndYield(String requestInfo) {
-        return "PumpStateAndYield";
+        String response = null;
+        System.out.println("**** Request Info ****");
+        System.out.println(requestInfo);
+        try {
+            RequestInfo requestInfoObj = JSONConverter.CovertToObject(requestInfo, RequestInfo.class);
+            System.out.println("**** Parsed Request Info ****");
+            System.out.println("Uid : " + requestInfoObj.getUid());
+            System.out.println("ChartType : " + requestInfoObj.getChartType());
+            System.out.println("FromDate : " + requestInfoObj.getFromDate());
+            System.out.println("ToDate : " + requestInfoObj.getToDate());
+
+            InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("PumpStateAndYield.json");
+            if (resourceAsStream != null) {
+                response = IOUtils.toString(resourceAsStream);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            response = e.getMessage();
+        }
+        System.out.println("**** Response Info ****");
+        System.out.println(response);
+        return response;
     }
 
     @Override
     public String getYieldByWaterSource(String requestInfo) {
-        return "YieldByWaterSource";
+        String response = null;
+        System.out.println("**** Request Info ****");
+        System.out.println(requestInfo);
+        try {
+            RequestInfo requestInfoObj = JSONConverter.CovertToObject(requestInfo, RequestInfo.class);
+            System.out.println("**** Parsed Request Info ****");
+            System.out.println("Uid : " + requestInfoObj.getUid());
+            System.out.println("ChartType : " + requestInfoObj.getChartType());
+            System.out.println("FromDate : " + requestInfoObj.getFromDate());
+            System.out.println("ToDate : " + requestInfoObj.getToDate());
+
+            InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("YieldByWaterSource.json");
+            if (resourceAsStream != null) {
+                response = IOUtils.toString(resourceAsStream);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            response = e.getMessage();
+        }
+        System.out.println("**** Response Info ****");
+        System.out.println(response);
+        return response;
     }
 }
