66c3b4f187fc62c01d997741f23393fd30097a6a
Karthik <karthik131289@gmail.com>
2018-09-16 20:24:58 +0530

    CORS issue fixed final. cors module added as dependency.

--- VenAquaREST/app.js
+++ VenAquaREST/app.js
@@ -27,22 +27,23 @@ var server = app.listen(3000, function () {
  
 var soap = require("soap");
 var express = require("express");
+var cors = require("cors");
 var bodyParser = require("body-parser");
 
 var app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-var routes = require("./routes/routes.js")(app, soap);
-
-var server = app.listen(process.env.PORT || 3000, function () {
-    console.log("Listening on port %s...", server.address().port);
-});
-
-app.use(function(req, res, next) {
+/* app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
+}); */
+
+var routes = require("./routes/routes.js")(app, soap);
+
+var server = app.listen(process.env.PORT || 3000, function () {
+    console.log("Listening on port %s...", server.address().port);
 });
