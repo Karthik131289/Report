074039bae21fa8659f444f6b593484d9b29c7048
karthik <karthik.p@volantetech.net>
2018-09-18 17:04:01 +0530

    RequestInfo.java modified

--- venaquareport/src/main/java/com/wegot/venaqua/report/ws/RequestInfo.java
+++ venaquareport/src/main/java/com/wegot/venaqua/report/ws/RequestInfo.java
@@ -8,15 +8,21 @@ public class RequestInfo {
     private String chartType;
     private Date fromDate;
     private Date toDate;
+    private Date fromTime;
+    private Date toTime;
+    private Date date;
 
     public RequestInfo() {
 
     }
-    public RequestInfo(String uid, String chartType, Date fromDate, Date toDate) {
+    public RequestInfo(String uid, String chartType, Date fromDate, Date toDate, Date fromTime, Date toTime, Date date) {
         this.uid = uid;
         this.chartType = chartType;
         this.fromDate = fromDate;
         this.toDate = toDate;
+        this.fromTime = fromTime;
+        this.toTime = toTime;
+        this.date = date;
     }
 
     @XmlElement(required = true)
@@ -37,7 +43,7 @@ public class RequestInfo {
         this.chartType = chartType;
     }
 
-    @XmlElement(required = true)
+    @XmlElement(required = false)
     public Date getFromDate() {
         return fromDate;
     }
@@ -46,12 +52,39 @@ public class RequestInfo {
         this.fromDate = fromDate;
     }
 
-    @XmlElement(required = true)
+    @XmlElement(required = false)
     public Date getToDate() {
         return toDate;
     }
 
     public void setToDate(Date toDate) {
         this.toDate = toDate;
+    }
+
+    @XmlElement(required = false)
+    public Date getFromTime() {
+        return fromTime;
+    }
+
+    public void setFromTime(Date fromTime) {
+        this.fromTime = fromTime;
+    }
+
+    @XmlElement(required = false)
+    public Date getToTime() {
+        return toTime;
+    }
+
+    public void setToTime(Date toTime) {
+        this.toTime = toTime;
+    }
+
+    @XmlElement(required = false)
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
