a8f583c55538828565e5625071e59de080db2573
Karthik <karthik131289@gmail.com>
2018-10-14 22:16:42 +0530

    Connection timeout added and set to 1min. connect timeout dependency added.

--- VenAquaREST/app.js
+++ VenAquaREST/app.js
@@ -29,6 +29,7 @@ var soap = require("soap");
 var express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser");
+var timeout = require('connect-timeout');
 const PropertiesReader = require('properties-reader');
 
 /*** Read application.properties ***/
@@ -36,6 +37,8 @@ const prop = PropertiesReader('./resources/application.properties');
 
 var app = express();
 app.use(cors());
+app.use(timeout(60000));
+app.use(haltOnTimedout);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 /* app.use(function(req, res, next) {
@@ -51,3 +54,7 @@ var routes = require("./routes/routes.js")(app, soap, prop);
 var server = app.listen(process.env.PORT || 3000, function () {
     console.log("Listening on port %s...", server.address().port);
 });
+
+function haltOnTimedout(req, res, next){
+  if (!req.timedout) next();
+}
