f78e17767229fed499839cf8549c4abe3e320570
Karthik <karthik131289@gmail.com>
2018-10-17 18:27:29 +0530

    code optimized in app.js

--- VenAquaREST/app.js
+++ VenAquaREST/app.js
@@ -1,30 +1,3 @@
-/* var soap = require("soap");
-var express = require("express");
-var bodyParser = require("body-parser");
-
-var url = 'http://venaqua-report.herokuapp.com/report?wsdl';
-var soap_client;
-soap.createClient(url, function(err, client) {
-	if(err) {
-		console.error(err);
-	}
-	else {
-		console.log("soap client initiated..");
-		soap_client = client;
-	}
-});
-
-var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-var routes = require("./routes/routes.js")(app, soap_client);
-
-var server = app.listen(3000, function () {
-    console.log("Listening on port %s...", server.address().port);
-});
- */
- 
 var soap = require("soap");
 var express = require("express");
 var cors = require("cors");
@@ -41,13 +14,6 @@ app.use(timeout(60000));
 app.use(haltOnTimedout);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-/* app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-}); */
 
 var routes = require("./routes/routes.js")(app, soap, prop);
 
