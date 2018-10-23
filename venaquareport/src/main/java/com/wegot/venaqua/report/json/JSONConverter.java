ce385731aaf7da3251bff13b3916c23716f346d6
karthik <karthik.p@volantetech.net>
2018-10-23 16:19:40 +0530

    jackson lib updated to latest.

--- venaquareport/src/main/java/com/wegot/venaqua/report/json/JSONConverter.java
+++ venaquareport/src/main/java/com/wegot/venaqua/report/json/JSONConverter.java
@@ -1,6 +1,6 @@
 package com.wegot.venaqua.report.json;
 
-import org.codehaus.jackson.map.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
