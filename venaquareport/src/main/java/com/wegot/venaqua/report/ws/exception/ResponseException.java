d3afc71462b8f80b05ea4d4e43654b4d5e12e21f
karthik <karthik.p@volantetech.net>
2018-10-23 17:15:29 +0530

    Added Exception classes.

---
+++ venaquareport/src/main/java/com/wegot/venaqua/report/ws/exception/ResponseException.java
@@ -0,0 +1,22 @@
+package com.wegot.venaqua.report.ws.exception;
+
+public class ResponseException extends Exception {
+    public ResponseException() {
+    }
+
+    public ResponseException(String message) {
+        super(message);
+    }
+
+    public ResponseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResponseException(Throwable cause) {
+        super(cause);
+    }
+
+    public ResponseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
